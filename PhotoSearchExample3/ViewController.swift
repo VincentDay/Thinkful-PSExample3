600deba0ce2310c81f00144271688b460bef1ebd
VincentDay <vincent.day@gmail.com>
2015-10-02 15:49:48 -0400

    //error push to Mike

--- PhotoSearchExample3/ViewController.swift
+++ PhotoSearchExample3/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+  
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+            }
 
+    override func viewDidAppear(animated: Bool) {
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=e311dd29d01840f8987e58bc3b31121c",
+            parameters: nil,
+            success:
+            {
+                (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                //print("JSON: " + responseObject.description)
+                
+                if let dataArray = responseObject["data"] as? [AnyObject]
+                {
+                    var urlArray:[String] = []                  //1
+                    for dataObject in dataArray
+                    {               //2
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String
+                        { //3
+                            urlArray.append(imageURLString)     //4
+                        }
+                    }
+                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    for var i = 0; i < urlArray.count; i++ {
+                        let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))     //1
+                        if let url = NSURL(string: urlArray[i]) {
+                            imageView.setImageWithURL( url)                          //2
+                            self.scrollView.addSubview(imageView)
+                        }                    }
+                }
+            },
+            
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                print("Error: " + error.localizedDescription)
+        })
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  PhotoSearchExample3
+//
+//  Created by Vince Day on 9/28/15.
+//  Copyright © 2015 Vince Day. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+
+#endif /* bridge_h */
