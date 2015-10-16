a8b91dc75aa0d49fe4ee942cec82b943d290b544
VincentDay <vincent.day@gmail.com>
2015-10-16 14:57:29 -0400

    //problem with search bar???

--- PhotoSearchExample3/ViewController.swift
+++ PhotoSearchExample3/ViewController.swift
@@ -18,50 +18,42 @@ class ViewController: UIViewController, UISearchBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchInstagramByHashtag("dogs")
         
         
             }
 
-    override func viewDidAppear(animated: Bool) {
+    //MARK: Utility methods
+    func searchInstagramByHashtag(searchString:String) {
         let manager = AFHTTPRequestOperationManager()
-        
-        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=e311dd29d01840f8987e58bc3b31121c",
+        manager.GET( "https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=c4fc61c4704949baab8825cf178e13fe",
             parameters: nil,
-            success:
-            {
-                (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-                //print("JSON: " + responseObject.description)
-                
-                if let dataArray = responseObject["data"] as? [AnyObject]
-                {
-                    var urlArray:[String] = []                  //1
-                    for dataObject in dataArray
-                    {               //2
-                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String
-                        { //3
-                            urlArray.append(imageURLString)     //4
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                if let dataArray = responseObject["data"] as? [AnyObject] {
+                    var urlArray:[String] = []
+                    for dataObject in dataArray {
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            urlArray.append(imageURLString)
                         }
                     }
+                    //display urlArray in ScrollView
                     self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    
                     for var i = 0; i < urlArray.count; i++ {
-                        let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))     //1
+                        let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))
                         if let url = NSURL(string: urlArray[i]) {
-                            print ("loading URL")
-                            imageView.setImageWithURL( url)                          //2
+                            imageView.setImageWithURL( url)
                             self.scrollView.addSubview(imageView)
-                            print ("added to subview")
                         }
                     }
-                
+                    
                 }
             },
-            
             failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 print("Error: " + error.localizedDescription)
         })
-
+        
     }
-    
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
         for subview in self.scrollView.subviews {
             subview.removeFromSuperview()
